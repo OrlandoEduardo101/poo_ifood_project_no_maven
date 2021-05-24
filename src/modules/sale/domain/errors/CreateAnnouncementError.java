b9e451c17a30855fb7a751cbcf79b782c8bd5661
Orlando Eduardo Pereira <orlando.eduardo101@gmail.com>
2021-05-23 22:50:01 -0300

    purchase module

--- src/modules/sale/domain/errors/CreateAnnouncementError.java
+++
@@ -1,9 +0,0 @@
-package modules.sale.domain.errors;
-
-import modules.home.domain.errors.IHomeException;
-
-public class CreateAnnouncementError extends IHomeException {
-    public CreateAnnouncementError(String errorMessage) {
-        super(errorMessage);
-    }
-}
